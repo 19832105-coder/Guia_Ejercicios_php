7c194116eca94b90f8180da7dcae90a13a6d8d8c
19832105-coder <19832105@clases.edu.sv>
2026-05-20 08:26:09 -0600

    Ejercicio5
    Realice un formulario que permita ingresar dos números y envíe los datos a otro
    archivo PHP donde se muestre el resultado de la división.

---
+++ Ejercicio5.php
@@ -0,0 +1,11 @@
+<?php
+echo"<center>";
+$numero1 =$_POST['numero1'];
+$numero2 = $_POST['numero2'];
+$resultado = $numero1 / $numero2;
+echo"El resultado de la division es: " . $resultado;echo"<br><br>"
+?>
+<a href="Ejercicio5.html"style="color:black">Volver al la division</a><br><br>
+<a href="index.php"style="color:black">Volver al indice</a>
+<body
+style="background-color:lightgray">
