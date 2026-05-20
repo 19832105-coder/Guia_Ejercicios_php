706d80cf2e6761235f642e522df54312530eebbe
19832105-coder <19832105@clases.edu.sv>
2026-05-20 08:22:31 -0600

    Ejercicio3
    Diseñe un formulario que permita ingresar dos números y envíe la información a
    otro archivo PHP para mostrar el resultado de la resta.

---
+++ Ejercicio3.php
@@ -0,0 +1,10 @@
+<?php
+$numero1 = $_POST['numero1'];
+$numero2 = $_POST['numero2'];
+$resultado = $numero1 - $numero2;
+echo "El resultado de la resta es: " . $resultado;
+echo"<br><br>";
+?>
+<a href="index.php" style="color: black;"> Volver al indice</a>
+<body
+style="background-color:lightgrey";>
