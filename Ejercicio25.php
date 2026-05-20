d65f89d70fa6f9f8292b1848f0dbc055d0f97ee3
19832105-coder <19832105@clases.edu.sv>
2026-05-20 08:54:48 -0600

    Ejercicio25
    Diseñe un formulario que solicite nombre, correo electrónico y ciudad del usuario.
    Los datos deben enviarse a otro archivo PHP donde se muestre toda la
    información ingresada.

---
+++ Ejercicio25.php
@@ -0,0 +1,18 @@
+<?php
+echo"<center><h3>";
+$nombre = $_POST["nombre"];
+$correo = $_POST["correo"];
+$ciudad = $_POST["ciudad"];
+echo"nombre: " . $nombre;
+echo "<br><br>";
+echo"correo: " . $correo;
+echo "<br><br>";
+echo "ciudad: ". $ciudad;
+echo "<br><br>";
+echo"<center><h3>";
+echo"<br><br>";
+?>
+<body
+style="background-color:lightgray">
+<a href="Ejercicio25.html">Volver</a><br><br>
+<a href="index.php">Volver al indice</a><br><br>
