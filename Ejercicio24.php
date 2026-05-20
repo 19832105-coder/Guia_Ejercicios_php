cae1b7a909c0eb897863061a1f3d6072143102c3
19832105-coder <19832105@clases.edu.sv>
2026-05-20 08:54:14 -0600

    Ejercicio24
    Elabore un formulario que solicite la edad actual de una persona y envíe la
    información a otro archivo PHP donde se muestre la edad que tendrá dentro de
    10 años.

---
+++ Ejercicio24.php
@@ -0,0 +1,13 @@
+<?php
+echo"<center><h3>";
+echo"<meta charset=UTF-8>";
+$edad = (float)$_POST["edad"];
+$futuro = $edad + 10;
+echo "Tienes " . $edad . " en 10 años tendras: " . $futuro;
+echo"</center></h3>";
+echo "<br><br>";
+?>
+<body
+style="background-color:lightgray">
+<a href="Ejercicio24.html">Volver</a><br><br>
+<a href="index.php">Volver al indice</a><br><br>
