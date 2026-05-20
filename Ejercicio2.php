f03e5e1a50a93cdc5ece84f754f01aeee07e47cf
19832105-coder <19832105@clases.edu.sv>
2026-05-20 08:20:54 -0600

    Ejercicio2
    Elabore un formulario que solicite dos números y que envíe los datos a otro archivo
    PHP donde se muestre el resultado de la suma.

---
+++ Ejercicio2.php
@@ -0,0 +1,10 @@
+<?php
+$numero1 = $_POST['numero1'];
+$numero2 = $_POST['numero2'];
+$resultado = $numero1 + $numero2;
+echo"El resultado es: " . $resultado;
+echo"<br><br>";
+?>
+<body
+style="background-color:lightblue;">
+<a href="index.php" style="color: white;">Volver al indice</a>
