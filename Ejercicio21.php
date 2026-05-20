01508a3e9a4a69e0401b605ab891b77e777398dc
19832105-coder <19832105@clases.edu.sv>
2026-05-20 08:51:48 -0600

    Ejercicio21
    Elabore un formulario que solicite una palabra y envíe la información a otro archivo
    PHP donde se muestre la cantidad de letras que contiene la palabra ingresada.

---
+++ Ejercicio21.php
@@ -0,0 +1,13 @@
+<?php
+echo"<h2>";
+$palabra = $_POST['palabra'];
+$letras = strlen($palabra);
+
+echo "La palabra '" . $palabra . "' tiene " . $letras . " letras";
+echo "<br><br>";
+
+?>
+<body
+style="background-color:lightgray">
+<a href="Ejercicio21.html">Volver</a><br><br>
+<a href="index.php">Volver al indice</a><br><br>
